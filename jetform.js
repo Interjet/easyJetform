f2dde3a24f67d8111ad348fa259a552b5ff33a2f
ronka <adir@interjet.co.il>
2016-12-18 14:20:07 +0200

    1.2.1

--- jetform.js
+++ jetform.js
@@ -109,7 +109,6 @@
 				// email validation
 				var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
 				if($(element).attr('type') == 'email'){
-					console.log($(element).val())
 					if( !re.test($(element).val()) ){
 						isValid = notValid('inCorrectEmail' ,$(element));
 						return;
